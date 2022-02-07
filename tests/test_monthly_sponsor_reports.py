3dc15f186408e32c2e15befc0065f929347e699d
jdh4 <52128661+jdh4@users.noreply.github.com>
2022-02-07 17:06:21 -0500

    Update test_monthly_sponsor_reports.py

--- tests/test_monthly_sponsor_reports.py
+++ tests/test_monthly_sponsor_reports.py
@@ -6,6 +6,7 @@ from sponsor import sponsor_full_name
 from sponsor import sponsor_per_cluster
 import monthly_sponsor_reports as msr
 
+
 class TestSponsorName(unittest.TestCase):
 
   def test_sponsor_name(self):
@@ -27,6 +28,7 @@ class TestSponsor(unittest.TestCase):
     d = {"della":None, "stellar":None, "tiger":None, "traverse":None, "displayname":None}
     assert sponsor_per_cluster(netid="bigfoot", verbose=False) == d
 
+
 class TestMonthlySponsorReports(unittest.TestCase):
 
   def test_gpus_per_job(self):
@@ -36,7 +38,7 @@ class TestMonthlySponsorReports(unittest.TestCase):
 
   def test_add_new_and_derived_fields(self):
     jobs = [[ 100, 100, "billing=8,cpu=1,mem=16G,node=1", 0, 123456789, 0],
-            [ 200, 100, "billing=8,cpu=2,gres/gpu=1,mem=16G,node=1", 1, 123456789, 100], 
+            [ 200, 100, "billing=8,cpu=2,gres/gpu=1,mem=16G,node=1", 1, 123456789, 100],
             [1600, 200, "billing=8,cpu=8,gres/gpu=4,mem=16G,node=1", 4, 123456789, 800]]
     df = pd.DataFrame(jobs)
     df.columns = ["cpu-seconds", "elapsedraw", "alloctres", "gpus", "start", "gpu-seconds"]
@@ -47,11 +49,33 @@ class TestMonthlySponsorReports(unittest.TestCase):
     expected = pd.Series([100, 0, 0]).rename("cpu-only-seconds")
     pd.testing.assert_series_equal(result["cpu-only-seconds"], expected)
 
-  #def test_double_groupby(self):
-  #  fields = "jobid,user,cluster,account,partition,cputimeraw,elapsedraw,timelimitraw,nnodes,ncpus,alloctres,submit,eligible,start"
-  #  renamings = {"user":"netid", "cputimeraw":"cpu-seconds", "nnodes":"nodes", "ncpus":"cores", "timelimitraw":"limit-minutes"}
-  #  numeric_fields = ["cpu-seconds", "elapsedraw", "limit-minutes", "nodes", "cores", "submit", "eligible"]
-  #  df = [["12345"]
-  #  cols = ["cluster", "sponsor", "netid", "name", "cpu-hours", "gpu-hours", "jobs", "account", "partition"]
-  #  msr.double_groupby(df) == expected
-~                                               
+  def test_double_groupby(self):
+    # construct simulated sacct output
+    jobs = [[ 100, "jdh4",     "tiger", "cses", "cpu", 25000, 50000, 100000, 1, 1, "billing=8,cpu=1,mem=16G,node=1",            123456789, 234567890, 234567890],
+            [ 101, "jdh4",     "tiger", "cses", "cpu", 20000, 10000, 100000, 1, 2, "billing=8,cpu=2,mem=16G,node=1",            123456789, 234567890, 234567890],
+            [ 102, "jdh4",     "tiger", "cses", "gpu", 20000, 10000, 100000, 1, 2, "billing=8,cpu=2,gres/gpu=1,mem=16G,node=1", 123456789, 234567890, 234567890],
+            [ 103, "gbwright", "tiger", "cses", "gpu", 80000, 20000, 100000, 1, 4, "billing=8,cpu=4,gres/gpu=4,mem=16G,node=1", 123456789, 234567890, 234567890],
+            [ 104, "bill",     "della", "cses", "cpu", 30000, 10000, 100000, 1, 3, "billing=8,cpu=3,mem=16G,node=1",            123456789, 234567890, 234567890]]
+    df = pd.DataFrame(jobs)
+    fields = "jobid,netid,cluster,account,partition,cpu-seconds,elapsedraw,limit-minutes,nodes,cores,alloctres,submit,eligible,start"
+    df.columns = fields.split(",")
+    df = msr.add_new_and_derived_fields(df)
+    # construct the expected result
+    expected = [["della", "curt", "bill", "William Wichser",     round(30000/3600),           0,                   1, "cses", "cpu"],
+                ["tiger", "curt", "jdh4", "Jonathan Halverson",  round((25000+2*20000)/3600), round(10000/3600),   3, "cses", "cpu,gpu"],
+                ["tiger", "wtang", "gbwright", "Garrett Wright", round(4*20000/3600),         round(4*20000/3600), 1, "cses", "gpu"]]
+    expected = pd.DataFrame(expected)
+    cols = ["cluster", "sponsor", "netid", "name", "cpu-hours", "gpu-hours", "jobs", "account", "partition"]
+    expected.columns = cols
+    expected["cpu-hours"] = expected["cpu-hours"].astype("int64")
+    expected["gpu-hours"] = expected["gpu-hours"].astype("int64")
+    # get the actual result
+    d1 = {"della":"curt", "stellar":"curt", "tiger":"wtang", "traverse":"curt", "displayname":"Garrett Wright"}
+    d2 = {"della":"curt", "stellar":"curt", "tiger":"curt", "traverse":"curt", "displayname":"Jonathan D. Halverson"}
+    d3 = {"della":"curt", "stellar":"curt", "tiger":"curt", "traverse":"curt", "displayname":"William Wichser"}
+    user_sponsor = [["gbwright", d1], ["jdh4", d2], ["bill", d3]]
+    user_sponsor = pd.DataFrame(user_sponsor)
+    user_sponsor.columns = ["netid", "sponsor-dict"]
+    actual = msr.groupby_cluster_netid_and_get_sponsor(df, user_sponsor)[cols].reset_index(drop=True)
+    # compare the two dataframes
+    pd.testing.assert_frame_equal(actual, expected)
