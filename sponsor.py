3ab89807cfca2cb6f392854183663cedd6b971d6
jdh4 <52128661+jdh4@users.noreply.github.com>
2023-02-19 14:13:21 -0500

    Changed function names; added functions that use log file

--- sponsor.py
+++ sponsor.py
@@ -64,64 +64,68 @@ def strip_accents(s):
   return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
 
 
-def sponsor_full_name(netid, verbose=True):
+def sponsor_full_name(netid, verbose=True, strip_accents=True):
   """Return the full name of the sponsor for the given netid of the sponsor."""
   cmd = f"ldapsearch -x -H ldap://ldap01.rc.princeton.edu -b dc=rc,dc=princeton,dc=edu uid={netid} displayname"
   output = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, timeout=5, text=True, check=True)
   lines = output.stdout.split('\n')
-  if lines != [] and lines[-1] == "": lines = lines[:-1]
-
   displayname = None
   for line in lines:
     if "displayname:: " in line:
-      displayname = line.split(":: ")[1].strip()
-      displayname = strip_accents(base64.b64decode(displayname).decode("utf-8"))
+      rawname = line.split(":: ")[1].strip()
+      displayname = base64.b64decode(rawname).decode("utf-8")
+      return strip_accents(displayname) if strip_accents else displayname
     if "displayname: " in line:
       displayname = line.split(": ")[1].strip()
+      return displayname
   if displayname is None and verbose: print(f"W: Name not found in CSES LDAP for sponsor {netid}.")
   return displayname
 
 
-def get_full_name_of_user(netid):
+def get_full_name_of_user(netid, strip_accents=True):
+  """Return the full name of the user by calling ldapsearch."""
   cmd = f"ldapsearch -x uid={netid} displayname"
   output = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, timeout=5, text=True, check=True)
   lines = output.stdout.split('\n')
   for line in lines:
     if "displayname:: " in line:
-      full_name = line.split(":: ")[1].strip()
-      full_name = strip_accents(base64.b64decode(full_name).decode("utf-8"))
-      if full_name.replace(".", "").replace(",", "").replace(" ", "").replace("-", "").isalpha():
-        return f"{full_name} ({netid})"
+      raw_name = line.split(":: ")[1].strip()
+      full_name = base64.b64decode(raw_name).decode("utf-8")
+      if strip_accents: full_name = strip_accents(full_name)
+      return f"{full_name} ({netid})"
     if line.startswith("displayname:"):
       full_name = line.replace("displayname:", "").strip()
+      # consider removing next line since b64 is now handled properly
       if full_name.replace(".", "").replace(",", "").replace(" ", "").replace("-", "").isalpha():
         return f"{full_name} ({netid})"
   return netid
 
 
-def get_missing_name_from_log(netid):
+def get_name_of_user_from_log(netid):
+  """Return the full name of the user from the log file."""
   with open("tigress_user_changes.log", "r") as f:
-      data = f.readlines()
+    data = f.readlines()
   pattern = f" {netid} "
   logname = None
   for line in data:
-      if pattern in line:
-          if f" Added user {netid} (" in line:
-              logname = line.split(f" Added user {netid} (")[-1].split(")")[0].split(" - ")[-1]
-          if f" Removed user {netid} (" in line:
-              logname = line.split(f" Removed user {netid} (")[-1].split(")")[0]
+    if pattern in line:
+      if f" Added user {netid} (" in line:
+        logname = line.split(f" Added user {netid} (")[-1].split(")")[0].split(" - ")[-1]
+      if f" Removed user {netid} (" in line:
+        logname = line.split(f" Removed user {netid} (")[-1].split(")")[0]
   return logname
 
 
-def get_missing_sponsor_from_log(netid):
+def get_sponsor_netid_of_user_from_log(netid):
+  """Return the sponsor netid for a given user netid from the log file."""
   with open("tigress_user_changes.log", "r") as f:
-      data = f.readlines()
+    data = f.readlines()
   pattern = f" {netid} "
   sponsor = None
   for line in data:
-      if pattern in line:
-          if f" Added user {netid} " in line and " with sponsor " in line:
-              sponsor = line.split(" with sponsor ")[-1].split()[0]
-          if f" Removed user {netid} " in line and " sponsor " in line:
-              sponsor = line.split(" sponsor ")[-1].split(";")[0]
+    if pattern in line:
+      if f" Added user {netid} " in line and " with sponsor " in line:
+        sponsor = line.split(" with sponsor ")[-1].split()[0]
+      if f" Removed user {netid} " in line and " sponsor " in line:
+        sponsor = line.split(" sponsor ")[-1].split(";")[0]
   return sponsor
